711b849519f94f879e740cefd41e674c29910c0e
Mechpet <blazepet2@gmail.com>
2022-07-04 18:40:07 -0500

    Added addition feature and some kind of removal

--- popup.js
+++ popup.js
@@ -1,10 +1,18 @@
 "use strict";
 
-function appendItem() {
-    $("ul").append("<li>Empty</li>");
+function appendItem(event) {
+    event.preventDefault();
+    $("ul").append("<li>" + $("input[name = quote]").val() + "</li>");
+}
+
+function removeItem(event) {
+    //event.preventDefault();
+    $(this).parent().remove()
 }
 
 // Run-time function
 $(function() {
     $("button").click(appendItem)
+
+    $("li").click(removeItem)
 })
