d187e3a0dd5c625043ed3c18f1711fda5f784ba3
Mechpet <blazepet2@gmail.com>
2022-07-06 21:24:57 -0500

    Added initializing of keyobjects

--- popup.js
+++ popup.js
@@ -10,30 +10,33 @@ function appendItem(event) {
     ul.appendChild(li.firstChild);
     // Attach click event back to all list items
     $("li").on("click", removeItem);
-
-    // Add item to storage
 }
 
 /* Asynchonous method to get setting and set setting using closure from the caller getSaved() */
-function getKeyValue(keyName, callback, element) {
+function getKeyValue(keyName, callback, ...args) {
     chrome.storage.sync.get(keyName, function(keyObj) {
-        callback(keyObj, element);
+        // Spread arguments (split back to ...args of callback)
+        return callback(keyObj, ...args);
     });
 }
 
 function removeItem(event) {
+    // Remove a list item (this) from the unordered list
     var ul = document.getElementsByTagName("ul")[0];
     ul.removeChild(this);
 }
 
-function checkValue(keyObj, element) {
-    if (Object.keys(keyObj).length === 0)
-        console.log("True");
-    else
-        console.log("False");
+function setIfEmpty(keyObj, ...args) {
+    // args[0] : key
+    // args[1] : value
+    // Initializes a key-value pair if it does not exist in Chrome storage
+    if (Object.keys(keyObj).length === 0) {
+        setKeyValue(args[0], args[1]);
+    }
 }
 
 function setKeyValue(keyName, value) {
+    // Set a keyobject in Chrome storage
     let keyObj;
     keyObj = {};
     keyObj[keyName] = value;
@@ -42,11 +45,21 @@ function setKeyValue(keyName, value) {
     });
 }
 
+function rmvKeyValue(keyName) {
+    // Remove a keyobject from Chrome storage (all attempts to get it will return `undefined`)
+    chrome.storage.sync.remove(keyName, function() {
+        console.log("Removed setting w/ key name: ", keyName);
+    });
+}
+
 // Run-time function
 $(function() {
+    // Initialize number of quotes to 0 if the setting does not exist
+    getKeyValue("number", setIfEmpty, "number", 0);
+    getKeyValue("keys", setIfEmpty, "keys", []);
+
     // Attach event functions to the initial elements
     $("button").click(appendItem);
     $("li").click(removeItem);
-    setKeyValue("number", "0");
-    getKeyValue("number", checkValue);
+    
 })
